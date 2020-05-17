78cea6f3b03722ca104a3cd4a0a44fd04922ee73
tndms951 <tndms951@naver.com>
2020-05-17 15:49:14 +0900

    test

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,37 +1,32 @@
 import React, { Component } from 'react';
 import './Header.css';
 
-
-
 // const Header = (props) => (
 //     <div className="nav">
 //         <h1> hellojisu</h1>
-//     </div> 
+//     </div>
 // )
 
 class Header extends Component {
-    render() {
-        return (
-            <div className="nav">
-                <div className="logo">
-                <a href="#">
-                    JISU
-                </a>
-                </div>
-                <div className="menu">
-                <a href="#">
-                    <span>MENU</span>
-                    <span>
-                        <b className="a"></b>
-                        <b className="b"></b>
-                        <b className="c"></b>
-                    </span>
-                </a>
-                </div>
-            </div>
-           
-        )
-    }
+  render() {
+    return (
+      <div className="nav">
+        <div className="logo">
+          <a href="#">JISU1</a>
+        </div>
+        <div className="menu">
+          <a href="#">
+            <span>MENU</span>
+            <span>
+              <b className="a"></b>
+              <b className="b"></b>
+              <b className="c"></b>
+            </span>
+          </a>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Header;
