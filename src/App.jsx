1fc13331263a50ff6370e3328ab55a20863d561a
tndms951 <tndms951@naver.com>
2020-05-17 15:40:58 +0900

    메인페이지구조추가

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react';
+import Header from './components/Header/Header';
+import Text from './components/content/Text';
+
+function App(props) {
+  return (
+    <div>
+      <Header />
+      <Text />
+    </div>
+  );
+}
+export default App;
---
+++ src/components/content/Text.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import './Text.css';
+
+class Text extends Component {
+    render() {
+        return (
+            <div className="text_wrap">
+                <span>
+                <i className="in">
+                    <em>F</em>
+                </i>
+                <i className="in">
+                    <em>R</em>
+                </i>
+                <i className="in">
+                    <em>E</em>
+                </i>
+                <i className="in">
+                    <em>N</em>
+                </i>
+                <i className="in">
+                    <em>T</em>
+                </i>
+                <i className="in">
+                    <em>E</em>
+                </i>
+                <i className="in">
+                    <em>N</em>
+                </i>
+                <i className="in">
+                    <em>D</em>
+                </i>
+                <i className="in box">
+                    <em></em>
+                </i>
+                </span>
+                
+                {/* <span>
+                    <i className="out">
+                        <em>D</em>
+                    </i>
+                    <i className="out">
+                        <em>E</em>
+                    </i>
+                    <i className="out">
+                        <em>V</em>
+                    </i>
+                    <i className="out">
+                        <em>E</em>
+                    </i>
+                    <i className="out">
+                        <em>L</em>
+                    </i>
+                    <i className="out">
+                        <em>O</em>
+                    </i>
+                    <i className="out">
+                        <em>P</em>
+                    </i>
+                    <i className="out">
+                        <em>E</em>
+                    </i>
+                    <i className="out">
+                        <em>R</em>
+                    </i>
+                    <i className="out box2">
+                        <em></em>
+                    </i>
+                    
+                </span> */}
+
+                <div className="under_text">
+                    <h5> Hello, This is my personal portfolio area.</h5>
+                </div>
+            </div>
+        )
+    }
+}
+export default Text;
